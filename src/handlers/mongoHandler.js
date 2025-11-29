909cff3b14796e432a8d83ede8270861f86a58db
Giovanni Pereira <80793459+giovannipereiradev@users.noreply.github.com>
2025-11-29 02:29:13 -0300

    refactor: optimize code and update comments for clarity

--- src/handlers/mongoHandler.js
+++ src/handlers/mongoHandler.js
@@ -1,14 +1,29 @@
-import mongoose from 'mongoose';
-import { log } from '../services/logger.js';
-import { loadConfig } from '../../config.js';
+import mongoose from "mongoose";
+import { log } from "../services/logger.js";
+import { loadConfig } from "../../config.js";
 
 const config = await loadConfig();
 
+/**
+ * Connects to MongoDB using the URL defined in the environment variables.
+ * If the URL is missing or invalid, the connection is skipped.
+ *
+ * @async
+ * @function connectMongo
+ * @returns {Promise<void>}
+*/
+
 export async function connectMongo() {
+  // Skip connection if URL is missing
+  if (!config.mongoUri) {
+    log.warn("MongoDB connection skipped: no valid connection URL.");
+    return;
+  }
+
   try {
     await mongoose.connect(config.mongoUri);
-    log.info('MongoDB connected');
+    log.info("MongoDB connected");
   } catch (error) {
-    log.error(`Error MongoDB: ${error.message}`);
+    log.error(`MongoDB connection error: ${error.message}`);
   }
 }
