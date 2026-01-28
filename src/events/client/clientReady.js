5d6d87dae76788a625843cfc9f092ee244084613
Giovanni Pereira <80793459+giovannipereiradev@users.noreply.github.com>
2026-01-28 14:25:53 -0300

    feat: add startPresenceRotator

--- src/events/client/clientReady.js
+++ src/events/client/clientReady.js
@@ -1,4 +1,5 @@
 import { log } from '../../services/logger.js';
+import { startPresenceRotator } from '../../utils/presenceRotator.js';
 
 /**
  * Handles the Discord "ready" event.
@@ -10,4 +11,7 @@ import { log } from '../../services/logger.js';
 
 export default async (client) => {
   log.success(`${client.user.tag} online`);
+
+  // Start rotating presence messages
+  startPresenceRotator(client, 5000); 
 };
